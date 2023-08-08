3485c1f403583d4dcf747063fb0ddf9e5789a2ca
Datta Magar <dattamagar211@gmail.com>
2023-08-08 17:09:10 +0530

    Updated pandas (codes+notes)

---
+++ pandas-fundamentals.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import numpy as np
+import pandas as pd 
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# In[ ]:
+
+
+#df = pd.read_csv('C:/Users/DAMAGAR/Downloads/pandas-fundamentals/demos/collection-master/artwork_data.csv', nrows=10)
+#df
+
+
+# In[32]:
+
+
+# df.head()
+# df.tail()
+
+
+# In[ ]:
+
+
+# df = pd.read_csv('C:/Users/DAMAGAR/Downloads/pandas-fundamentals/demos/collection-master/artwork_data.csv', 
+#                  index_col='id', usecols=['id','artist'])
+# df.head()
+
+
+# In[ ]:
+
+
+# COLS_TO_USE = ['id','artist','title','medium','year','acquisitionYear','height','width','units']
+# CSV_PATH = 'C:/Users/DAMAGAR/Downloads/pandas-fundamentals/demos/collection-master/artwork_data.csv'
+# df = pd.read_csv(CSV_PATH, usecols=COLS_TO_USE, index_col='id')
+# df.head()
+
+
+# In[ ]:
+
+
+
+
